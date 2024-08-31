fe43ccb3eb43c0324252703bef89858d45c54a1e
matheusleite01 <de.leite2014@gmail.com>
2024-08-31 15:55:24 -0300

    feat: execute connect Database

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import connectDatabase from "./db/dbConfig";
+import routes from "./routes";
+
+connectDatabase();
 
 const app = express();
-
+routes(app);
 
 export default app;

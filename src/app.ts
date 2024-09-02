fdc92dc79a9a2243f0f2f2dde4a180d4c900ce53
matheusleite01 <de.leite2014@gmail.com>
2024-09-02 17:36:08 -0300

    feat: add  CORS middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import connectDatabase from "./db/dbConfig";
 import routes from "./routes";
+import cors from "cors";
+
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://mars-rover-front-mu.vercel.app"],
+  optionsSuccessStatus: 200,
+};
 
 connectDatabase();
 
 const app = express();
+
+app.use(cors(corsOptions));
+
 routes(app);
 
 export default app;
